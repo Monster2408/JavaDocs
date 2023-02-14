75f36c508eb9e0ee604295325131135b0f16e54e
Monster2408 <meoto2408@gmail.com>
2023-02-14 23:42:04 +0900

    update

---
+++ Spigot-Update-Core/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"DownloadCallback","h":"","u":""},{"l":"ResourceFile","h":"","u":""},{"l":"ResourceInfo","h":"","u":""},{"l":"ResourceVersion","h":"","u":""},{"l":"SpigotUpdateAbstract","h":"","u":""},{"l":"UpdateCallback","h":"","u":""},{"l":"UpdateDownloader","h":"","u":""},{"l":"VersionComparator","h":"","u":""},{"l":"すべてのクラス","h":"","u":"allclasses-index.html"}]
---
+++ Spigot-Update-Core/versions/1.0.0/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"xyz.mlserver.spigotupdatecore","h":"","u":""},{"l":"xyz.mlserver.spigotupdatecore.comparator","h":"","u":""},{"l":"xyz.mlserver.spigotupdatecore.download","h":"","u":""},{"l":"すべてのパッケージ","h":"","u":"allpackages-index.html"}]
