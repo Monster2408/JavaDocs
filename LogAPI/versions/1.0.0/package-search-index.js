91d582649f959e9f1e4077c7be2a3dfd436dd3a2
Monster2408 <meoto2408@gmail.com>
2024-05-05 10:29:00 +0900

    add LogAPI docs

---
+++ LogAPI/versions/1.0.0/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"xyz.mlserver.logapi"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
---
+++ LogAPI/versions/1.0.0/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"xyz.mlserver.logapi","l":"LogAPI"},{"p":"xyz.mlserver.logapi","l":"LogAPI.Logger"}];updateSearchResults();
